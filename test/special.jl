d6e648896b748e076a1bdfd88c77de2cc5800206
JeremyBejanin <jbejanin@uwaterloo.ca>
2017-07-07 22:19:56 -0400

    make syntax 0.5 compatible

--- test/special.jl
+++ test/special.jl
@@ -38,7 +38,7 @@ end
     @test fidelity(basis(N,3),Operator(coherent(N,0.5α))) ≈ abs(coherent(N,0.5α)[4])
     @test fidelity2(Operator(coherent(N,0.5α)),basis(N,3)) ≈ abs2(coherent(N,0.5α)[4])
     @test fidelity2(basis(N,3),Operator(coherent(N,α))) ≈ abs2(coherent(N,α)[4])
-    @test fidelity(Operator(coherent(N,α)),Operator(basis(N,3))) ≈ abs(coherent(N,α)[4]) atol=1E-8
+    @test isapprox(fidelity(Operator(coherent(N,α)),Operator(basis(N,3))), abs(coherent(N,α)[4]), atol=1E-8)
     @test fidelity(Operator(basis(N,2)),Operator(coherent(N,1))) ≈ abs(coherent(N,1)[3])
     @test fidelity2(Operator(coherent(N,α,true)),Operator(basis(N,3))) ≈ abs2(coherent(N,α,true)[4])
     @test fidelity2(Operator(basis(N,2)),Operator(coherent(N,1))) ≈ abs2(coherent(N,1)[3])
