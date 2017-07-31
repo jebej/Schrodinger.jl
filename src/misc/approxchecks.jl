895b6eb51b99ab69f46750d6873f6feda86d8835
JeremyBejanin <jbejanin@uwaterloo.ca>
2017-07-31 18:21:49 -0400

    clean up approxherm functions

--- src/misc/approxchecks.jl
+++ src/misc/approxchecks.jl
@@ -1,20 +1,15 @@
-# The functions below check if a matrix is approximately hermitian, with a tolerance on the real and imaginary parts.
-const HERMERR = 1E-12
+# The functions below check if a matrix is approximately Hermitian
+const ERR = 1E-13
 
 function isapproxhermitian(A::AbstractMatrix)
-    N,M = size(A)
-    if N!==M
-        return false
-    end
-    @inbounds for j = 1:N
-        if abs(imag(A[j,j]))>HERMERR
-            return false
-        end
+    m, n = size(A)
+    m == n || return false
+    @inbounds for j = 1:m
         for i = 1:j-1
-            if abs(real(A[i,j])-real(A[j,i]))>HERMERR || abs(imag(A[i,j])+imag(A[j,i]))>HERMERR
-                return false
-            end
+            Aij, Aji = A[i,j], A[j,i]
+            abs(real(Aij)-real(Aji)) > ERR || abs(imag(Aij)+imag(Aji)) > ERR && return false
         end
+        abs(imag(A[j,j])) > ERR && return false
     end
     return true
 end
@@ -22,12 +17,12 @@ end
 function isapproxhermitian(A::SparseMatrixCSC)
     # Slighly modified from base
     m, n = size(A)
-    if m != n; return false; end
+    m == n || return false
     colptr = A.colptr
     rowval = A.rowval
     nzval = A.nzval
     tracker = copy(A.colptr)
-    @inbounds for col = 1:A.n
+    @inbounds for col = 1:m
         for p = tracker[col]:colptr[col+1]-1
             val = nzval[p]
             row = rowval[p]
@@ -43,9 +38,7 @@ function isapproxhermitian(A::SparseMatrixCSC)
             end
             # Diagonal element
             if row == col
-                if abs(imag(val))>HERMERR
-                    return false
-                end
+                abs(imag(val)) > ERR && return false
             else
                 offset = tracker[row]
                 # If the matrix is unsymmetric, there might not exist
@@ -72,9 +65,7 @@ function isapproxhermitian(A::SparseMatrixCSC)
                 end
                 # A[i,j] and A[j,i] exists
                 if row2 == col
-                    if abs(real(val)-real(nzval[offset]))>HERMERR || abs(imag(val)+imag(nzval[offset]))>HERMERR
-                        return false
-                    end
+                    abs(real(val)-real(nzval[offset])) > ERR || abs(imag(val)+imag(nzval[offset])) > ERR && return false
                     tracker[row] += 1
                 end
             end
@@ -83,8 +74,9 @@ function isapproxhermitian(A::SparseMatrixCSC)
     return true
 end
 
-function hermitianize!(A::AbstractMatrix, N=checksquare(A))
-    @inbounds for j = 1:N
+function hermitianize!(A::AbstractMatrix)
+    m = checksquare(A)
+    @inbounds for j = 1:m
         A[j,j] = real(A[j,j])
          for i = 1:j-1
             A[i,j] = conj(A[j,i])
