a0f5deb880bd18848ce9e398d652f648a514f865
JeremyBejanin <jbejanin@uwaterloo.ca>
2017-08-01 13:20:49 -0400

    fix promote_rule on 0.5

--- src/basepatch/v0.5.jl
+++ src/basepatch/v0.5.jl
@@ -3,5 +3,5 @@ import Base: promote_rule
 scale!(A::LinAlg.HermOrSym,b::Number) = (scale!(A.data,b);A)
 
 promote_rule{T1,T2,S1,S2}(::Type{SparseMatrixCSC{T1,S1}},::Type{SparseMatrixCSC{T2,S2}}) = SparseMatrixCSC{promote_type(T1,T2),promote_type(S1,S2)}
-promote_rule{T1,S1,T2,S2<:AbstractMatrix{T2}}(::Type{SparseMatrixCSC{T1,S1}},::Type{S2}) = Matrix{promote_type(T1,T2)}
-promote_rule{T1,S1,T2,S2<:AbstractMatrix{T2}}(::Type{S2},::Type{SparseMatrixCSC{T1,S1}}) = Matrix{promote_type(T1,T2)}
+promote_rule{T1,S1,S2<:AbstractMatrix}(::Type{SparseMatrixCSC{T1,S1}},::Type{S2}) = Matrix{promote_type(T1,eltype(S2))}
+promote_rule{T1,S1,S2<:AbstractMatrix}(::Type{S2},::Type{SparseMatrixCSC{T1,S1}}) = Matrix{promote_type(T1,eltype(S2))}
