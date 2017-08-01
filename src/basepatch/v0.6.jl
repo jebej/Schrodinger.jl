2d19d9fc2ee167324d2a6b801a96055f3307a4de
JeremyBejanin <jbejanin@uwaterloo.ca>
2017-08-01 13:08:26 -0400

    more promote rules for sparse matrices

--- src/basepatch/v0.6.jl
+++ src/basepatch/v0.6.jl
@@ -1,3 +1,7 @@
-Base.vec(x::RowVector) = x.vec
+import Base: vec, promote_rule
 
-Base.promote_rule{T1,T2,S1,S2}(::Type{SparseMatrixCSC{T1,S1}},::Type{SparseMatrixCSC{T2,S2}}) = SparseMatrixCSC{promote_type(T1,T2),promote_type(S1,S2)}
+vec(x::RowVector) = x.vec
+
+promote_rule{T1,T2,S1,S2}(::Type{SparseMatrixCSC{T1,S1}},::Type{SparseMatrixCSC{T2,S2}}) = SparseMatrixCSC{promote_type(T1,T2),promote_type(S1,S2)}
+promote_rule{T1,S1,T2,S2<:AbstractMatrix{T2}}(::Type{SparseMatrixCSC{T1,S1}},::Type{S2}) = Matrix{promote_type(T1,T2)}
+promote_rule{T1,S1,T2,S2<:AbstractMatrix{T2}}(::Type{S2},::Type{SparseMatrixCSC{T1,S1}}) = Matrix{promote_type(T1,T2)}
