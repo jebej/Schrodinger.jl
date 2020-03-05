6191fc014235ce6ac065ef5b1b0cdc5ee65cb5e9
Jeremy Bejanin <jeremy.bejanin@gmail.com>
2020-03-05 15:28:46 -0500

    further improve process tomo perf, now limited by eigendecomposition

--- src/tomography/processtomo.jl
+++ src/tomography/processtomo.jl
@@ -12,20 +12,20 @@ end
 # Quantum process tomography via completely positive and trace-preserving
 # projection. Phys. Rev. A 98, 062336 (2018).
 
-function pgd_process_tomo(M::Matrix, A::Matrix; tol=1E-10, cptp_tol=1E-8, info=false, cbfun=nothing)
+function pgd_process_tomo(M::Matrix{T}, A::Matrix; tol=1E-10, cptp_tol=1E-8, info=false, cbfun=nothing) where T
     # Choi process matrix reconstruction by maximum likelihood projected gradient descent
     size(A,1)==length(M) || throw(ArgumentError("A matrix inconsistent with number of measurements!"))
     abs(sum(M)-1)<1/4 || throw(ArgumentError("measurement counts not normalized!"))
     # infer space dimensions from A matrix
     d = isqrt(isqrt(size(A,2)))
     # initial Choi matrix guess, the identity map
-    C = Matrix{ComplexF64}(I/d,d^2,d^2)
+    C = Matrix{Complex{T}}(I/d,d^2,d^2)
     # objective and gradient functions setup
     f = C -> loglikelihood(M,C,A)
     ∇f = C -> loglikelihood_gradient(M,C,A)
     # metaparameters & initial cost calculation
-    μ = 3/2d^2; γ = 0.3
-    c₁ = 1E6; c₂ = f(C)
+    μ⁻¹ = 2d^2/T(3); γ = T(0.3)
+    c₁ = T(1E6); c₂ = f(C)
     info && println("start cost = $c₂")
     # iterate through projected gradient descent steps, with backtracking
     h = CPTP_helpers(C)
@@ -33,8 +33,8 @@ function pgd_process_tomo(M::Matrix, A::Matrix; tol=1E-10, cptp_tol=1E-8, info=f
     while c₁ - c₂ > tol
         stp += 1
         c₁, ∇C = c₂, ∇f(C)
-        D = project_CPTP(C .- 1/μ.*∇C, h, cptp_tol) - C
-        α = 1.0; Π = γ*real(D⋅∇C)
+        D = project_CPTP(C .- μ⁻¹.*∇C, h, cptp_tol) - C
+        α = one(T); Π = γ*real(D⋅∇C)
         while (c₂ = f(C .+ α.*D)) > c₁ + α*Π
             α = α/2 # backtrack
         end
@@ -47,27 +47,27 @@ function pgd_process_tomo(M::Matrix, A::Matrix; tol=1E-10, cptp_tol=1E-8, info=f
     return C
 end
 
-function loglikelihood(M::Matrix{T}, C::Matrix, A::Matrix) where {T<:Real}
+function loglikelihood(M::Matrix{T}, C::Matrix{Complex{T}}, A::Matrix) where {T<:Real}
     # Binomial statistics for the measurement count probability, up to some irrelevant constant
-    logP = log.(max.(real.(A*vec(C)), 1E-16))
-    return -real(vec(M)⋅logP)
+    logP = log.(max.(real.(A*vec(C)), T(1E-12)))
+    return -(M⋅logP)
 end
 
-function loglikelihood_gradient(M::Matrix, C::Matrix, A::Matrix)
-    P = max.(real.(A*vec(C)), 1E-16)
-    return unvec(-A'*(vec(M)./P))
+function loglikelihood_gradient(M::Matrix{T}, C::Matrix{Complex{T}}, A::Matrix) where {T<:Real}
+    mMP = .-vec(M)./max.(real.(A*vec(C)), T(1E-12))
+    return unvec(A'*mMP)
 end
 
-function project_CPTP(C::Matrix, h::Tuple, tol::Real=1E-8)
+function project_CPTP(C::Matrix{Complex{T}}, h::Tuple, tol::Real=1E-8) where {T<:Real}
     # generate storage objects
-    X₁ = copy(vec(C)); Y₁ = zero(X₁);
+    X₁ = copy(C); Y₁ = zero(X₁);
     X₂ = copy(Y₁); Y₂ = copy(Y₁)
     P = copy(Y₁); Q = copy(Y₁)
-    ΔP = 1.0; ΔQ = 1.0
-    D, V, Mdagvec𝕀, MdagM = h
+    ΔP = one(T); ΔQ = one(T)
+    D, V, MdagM, 𝕀 = h
     # iterate through TP & CP projections
     while ΔP^2 + ΔQ^2 + 2*abs(P⋅X₂-P⋅X₁) + 2*abs(Q⋅Y₂-Q⋅Y₁) > tol
-        project_TP!(Y₂, X₁+P, Mdagvec𝕀, MdagM)
+        project_TP!(Y₂, X₁+P, MdagM, 𝕀)
         ΔP = norm2_diff(X₁,Y₂)
         P .= X₁ .- Y₂ .+ P
         project_CP!(X₂, Y₂+Q, D, V)
@@ -76,40 +76,38 @@ function project_CPTP(C::Matrix, h::Tuple, tol::Real=1E-8)
         X₁, X₂ = X₂, X₁
         Y₁, Y₂ = Y₂, Y₁
     end
-    return unvec(X₁)
+    return X₁
 end
 
-function project_CP!(X::Vector, vecC::Vector, D::Vector, V::Matrix)
+function project_CP!(X::Matrix{Complex{T}}, C::Matrix{Complex{T}}, D::Vector{T}, V::Matrix) where {T<:Real}
     # Project the process onto the completely positive subspace by making the
     # Choi matrix positive semidefinite
     # We do this by taking the eigendecomposition, setting any negative
     # eigenvalues to 0, and reconstructing the Choi matrix
-    C = unvec(vecC)
-    hermitianize!(C)
-    hermfact!(D,V,Hermitian(C))
-    D .= max.(D,0)
+    hermfact!(D,V,Hermitian(hermitianize!(C)))
+    D .= max.(D,zero(T))
     mul!(C,Diagonal(D),V')
-    mul!(unvec(X),V,C)
+    mul!(X,V,C)
 end
 
-function project_TP!(Y::Vector, vecC::Vector, Mdagvec𝕀::Vector, MdagM::Matrix)
+function project_TP!(Y::Matrix{Complex{T}}, C::Matrix{Complex{T}}, MdagM::Matrix, 𝕀::Matrix) where {T<:Real}
     # Project the process onto the trace-preserving subspace
-    d⁻¹ = 1/isqrt(isqrt(length(vecC)))
-    mul!(Y, MdagM, vecC)
-    Y .= vecC .- d⁻¹.*Y .+ d⁻¹.*Mdagvec𝕀
+    d⁻¹ = one(T)/isqrt(size(C,1))
+    mul!(vec(Y), MdagM, vec(C))
+    Y .= C .- d⁻¹.*(Y.-𝕀)
 end
 
-function CPTP_helpers(C::Matrix)
-    D = Vector{real(eltype(C))}(undef,size(C,1))
-    V = Matrix{eltype(C)}(undef,size(C))
-    Mdagvec𝕀, MdagM = TP_helper_matrices(C)
-    return D, V, Mdagvec𝕀, MdagM
+function CPTP_helpers(C::Matrix{Complex{T}}) where {T<:Real}
+    D = Vector{T}(undef,size(C,1))
+    V = Matrix{Complex{T}}(undef,size(C))
+    MdagM, 𝕀 = TP_helper_matrices(C)
+    return D, V, MdagM, 𝕀
 end
 
-function TP_helper_matrices(C::Matrix)
+function TP_helper_matrices(C::Matrix{Complex{T}}) where {T<:Real}
     d = isqrt(size(C,1))
-    𝕀 = Matrix(1.0I,d,d); k = zeros(1,d)
-    # this can be done more efficiently, but prob doesn't matter
+    𝕀 = Matrix{T}(I,d,d); k = zeros(Complex{T},1,d)
+    # this could be done more efficiently, but it doesn't matter
     M = sum(i->(k[i]=1; k[mod1(i-1,d)]=0; (𝕀 ⊗ k) ⊗ (𝕀 ⊗ k)), 1:d)
-    return M'*vec(𝕀), M'*M
+    return M'*M, 𝕀⊗𝕀
 end
