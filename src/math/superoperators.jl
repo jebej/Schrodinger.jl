1b884e02db3650959452d2a0e3daf6b2154fc286
Jeremy Bejanin <jeremy.bejanin@gmail.com>
2020-03-05 17:28:20 -0500

    improve various superoperator functions

--- src/math/superoperators.jl
+++ src/math/superoperators.jl
@@ -1,28 +1,24 @@
-apply_process(C::Operator,ψ::Ket) = apply_process(C,Operator(ψ))
 # assumes Choi matrix
+apply_process(C::Operator,ψ::Ket) = apply_process(C,Operator(ψ))
+
 apply_process(C::Operator,ρ::Operator{T,D}) where {T,D} =
     ptrace((transpose(ρ)⊗qeye(dims(ρ)))*C,ntuple(identity,D))
+
 # assumes Kraus operators
 apply_process(As::Vector{<:Operator},ρ::Operator) = sum(A->A*ρ*A',As)
 
 function gate_fidelity_choi(C::Operator,U::Operator)
     # calculate the average gate fidelity given a Choi matrix and a unitary gate
-    if dims(C) != (dims(U)...,dims(U)...)
-        throw(DimensionMismatch())
-    end
+    dims(C) != (dims(U)...,dims(U)...) && throw(DimensionMismatch())
     V = qeye(dims(U)) ⊗ U
     return gate_fidelity_choi(V'*C*V)
 end
 
-function gate_fidelity_choi(C::Operator{T,D}) where {T,D}
+function gate_fidelity_choi(C::Operator)
     # calculate the average gate fidelity given a Choi matrix
     # Johnston, N. & Kribs, D. W. Quantum gate fidelity in terms of Choi
     # matrices. J. Phys. A Math. Theor. 44, 495303 (2011).
-    dm = dims(C)
-    if D%2 != 0 || prod(i->dm[i],1:D÷2) != prod(i->dm[i],D÷2+1:D)
-        throw(ArgumentError("not a valid Choi matrix!"))
-    end
-    d = prod(i->dm[i],1:D÷2)
+    d = check_choi(C)
     TrA² = let CC = Operator(data(C),(d,d)) # rewrap operator to use tensored indexing
         real(sum(CC[(i,i),(j,j)] for i=0:d-1, j=0:d-1))
     end
@@ -51,47 +47,40 @@ kraus_to_choi(As::Vector{<:Operator}) = sum(operator_to_choi,As)
 
 kraus_to_natural(As::Vector{<:Operator}) = sum(A->conj(A)⊗A,As)
 
-function natural_to_kraus(K::Operator)
-    # we go through the Choi representation since the extra conversion step is
-    # basically free compared to the eigendecomposition
-    return choi_to_kraus(natural_to_choi(K))
-end
+# go through Choi representation; the extra step is cheap compared to the eigendecomposition
+natural_to_kraus(K::Operator) = choi_to_kraus(natural_to_choi(K))
 
-function natural_to_choi(K::Operator)
-    # the conversion between the natural and Choi is a self-inverse
-    return choi_to_natural(K)
-end
+# the conversion between the natural and Choi is a self-inverse
+natural_to_choi(K::Operator) = choi_to_natural(K)
 
 function choi_to_natural(C::Operator)
     # the "natural" representation is the same as Choi with shuffled dimensions
-    length(dims(C)) == 2 || throw(ArgumentError("multi-space operators not supported yet!"))
-    d = dims(C)[1]
+    d = check_choi(C)
     K = reshape(permutedims(reshape(data(C),(d,d,d,d)),[1,3,2,4]),(d^2,d^2))
     return Operator(K,dims(C))
 end
 
 function choi_to_kraus(C::Operator)
-    length(dims(C)) == 2 || throw(ArgumentError("multi-space operators not supported yet!"))
+    check_choi(C)
     @static if VERSION < v"0.7.0-"
         @inbounds for i = 1:size(C,1); C[i,i] = real(C[i,i]); end
     end
     D,V = eigen(Hermitian(data(C)),1E-10,Inf)
-    return [Operator(unvec(√(D[i])*V[:,i]),(dims(C)[1],)) for i = length(D):-1:1]
+    dm = _half_dims(dims(C))
+    return [Operator(unvec(√(D[i])*V[:,i]),dm) for i = length(D):-1:1]
 end
 
-function choi_to_chi(C::Operator)
-    length(dims(C)) == 2 || throw(ArgumentError("multi-space operators not supported yet!"))
-    B = Operator(_choi_to_pauli_basis((dims(C)[1],)),dims(C))
-    return (B*C*B')/2^1 # TODO: fix for multi-qubit
+function choi_to_chi(C::Operator{T,D}) where {T,D}
+    B = Operator(_choi_to_pauli_basis(_half_dims(dims(C))),dims(C))
+    return (B*C*B')/2^(D÷2)
 end
 
-function chi_to_choi(χ::Operator)
-    length(dims(χ)) == 2 || throw(ArgumentError("multi-space operators not supported yet!"))
-    B = Operator(_choi_to_pauli_basis((dims(χ)[1],)),dims(χ))
-    return (B'*χ*B)/2^1 # TODO: fix for multi-qubit
+function chi_to_choi(χ::Operator{T,D}) where {T,D}
+    B = Operator(_choi_to_pauli_basis(_half_dims(dims(χ))),dims(χ))
+    return (B'*χ*B)/2^(D÷2)
 end
 
-function _choi_to_pauli_basis(D::NTuple{N,Int}) where N
+function _choi_to_pauli_basis(D::Dims{N}) where N
     all(d->d==2,D) || throw(ArgumentError("only valid with qubits!"))
     B = Matrix{Float64}(undef,4^N,4^N)
     # change to I,X,Y,Z basis (Y = -i*σy)
@@ -104,10 +93,13 @@ end
 
 Base.vec(O::Operator) = Ket(vec(data(O)),(dims(O)...,dims(O)...))
 
-unvec(v::Ket{T,D}) where {T,D} = Operator(copy(unvec(data(O))),dims(v)[1:D÷2])
+unvec(v::Ket) = Operator(unvec(data(v)),_half_dims(dims(v)))
 
 super(A::Operator) = conj(A) ⊗ A # represents A*ρ*A†
 super(A::Operator,B::Operator) = transpose(B) ⊗ A
+
+# linear operator that represents the action AρB on a vectorized version of ρ
+super(A::AbstractMatrix,B::AbstractMatrix=data(qeye(size(A,1)))) = transpose(B) ⊗ A
 
 function unvec(vecA::AbstractVector)
     # unvectorize a vector into a square matrix
@@ -115,7 +107,12 @@ function unvec(vecA::AbstractVector)
     return reshape(vecA,(d,d))
 end
 
-function super(A::AbstractMatrix,B::AbstractMatrix=data(qeye(size(A,1))))
-    # represents the action AρB, on a vectorized version of ρ
-    return transpose(B) ⊗ A
+function check_choi(C::Operator{T,D}) where {T,D}
+    dm = dims(C)
+    if D%2 != 0 || prod(i->dm[i],1:D÷2) != prod(i->dm[i],D÷2+1:D)
+        throw(ArgumentError("not a valid Choi matrix!"))
+    end
+    return prod(i->dm[i],1:D÷2)
 end
+
+_half_dims(dm::Dims{D}) where{D} = ntuple(i->dm[i],D÷2)
